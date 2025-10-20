fab137232eaca9cddab4f0e89db393ec18c7162f
oscar sanchez <120154684+D1052023@users.noreply.github.com>
2025-10-19 20:17:13 -0500

    prod
    Fixed comment formatting in AdminInitializer class.

--- src/main/java/eci/edu/dosw/proyecto/configs/AdminInitializer.java
+++ src/main/java/eci/edu/dosw/proyecto/configs/AdminInitializer.java
@@ -14,7 +14,7 @@ import eci.edu.dosw.proyecto.services.AuthUserService;
 
 /**
  * Clase de configuración que se ejecuta al iniciar la aplicación y garantiza que exista el 
- * usuario ADMIN por defecto en la base de datos.
+ * usuario ADMIN por defecto en la base de datos
  */
 @Configuration
 @RequiredArgsConstructor
