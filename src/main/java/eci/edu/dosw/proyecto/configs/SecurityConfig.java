fcc214f60a2a13dd2d98b8152b0aafe10b5d4e90
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-10-28 22:55:05 -0500

    featrue - preprod

--- src/main/java/eci/edu/dosw/proyecto/configs/SecurityConfig.java
+++ src/main/java/eci/edu/dosw/proyecto/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package eci.edu.dosw.proyecto.configs;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import eci.edu.dosw.proyecto.services.JwtService;
 import eci.edu.dosw.proyecto.enums.Role;
 import org.springframework.http.HttpMethod;
@@ -27,6 +33,22 @@ public class SecurityConfig {
         return new JwtAuthFilter(jwtService);
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "https://pawpatrol-front.vercel.app", // sin barra final
+                "http://localhost:5173"
+        ));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
