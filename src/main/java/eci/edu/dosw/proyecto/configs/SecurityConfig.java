6099bcc65df511eb6b00ec60371661e1a31cb3c1
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-10-28 22:56:31 -0500

    featrue - preprod

--- src/main/java/eci/edu/dosw/proyecto/configs/SecurityConfig.java
+++ src/main/java/eci/edu/dosw/proyecto/configs/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://pawpatrol-front.vercel.app", // sin barra final
+                "https://pawpatrol-front.vercel.app", 
                 "http://localhost:5173"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
